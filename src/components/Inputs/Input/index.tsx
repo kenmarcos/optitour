8bb47c3ea8e35bf2b9a22881d0e4e15b81cdd545
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-07-09 19:26:29 -0300

    styles: add styles in the input component

--- src/components/Inputs/Input/index.tsx
+++ src/components/Inputs/Input/index.tsx
@@ -12,7 +12,8 @@ const Input = (
   ref: LegacyRef<HTMLInputElement> | undefined
 ) => {
   const inputClassName = twMerge(
-    "border border-gray-primary rounded-lg bg-white p-2 text-sm font-normal text-gray-dark placeholder-black placeholder-opacity-20 outline-none",
+    "border border-gray-primary rounded-lg bg-white p-2 text-sm font-normal text-gray-dark placeholder-black placeholder-opacity-20 outline-none transition-all focus:ring-1 focus:ring-primary",
+    error ? "border-red-primary" : "",
     className
   );
 
