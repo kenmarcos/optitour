e9ce7e8cd70a026a002459476e841044c77f6079
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-07-10 14:31:16 -0300

    feat: add initial recommended trips section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import QuickSearch from "./components/QuickSearch";
+import RecommendedTrips from "./components/RecommendedTrips";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <QuickSearch />
+      <RecommendedTrips />
     </main>
   );
 }
