9d7c320e64bb40107e90a7ddb04aa261f794dd5b
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-07-09 21:49:19 -0300

    style: adjust the styles of input components

--- src/components/Inputs/Input/index.tsx
+++ src/components/Inputs/Input/index.tsx
@@ -12,7 +12,7 @@ const Input = (
   ref: LegacyRef<HTMLInputElement> | undefined
 ) => {
   const inputClassName = twMerge(
-    "border border-gray-primary rounded-lg bg-white p-2 text-sm font-normal text-gray-dark placeholder-black placeholder-opacity-20 outline-none transition-all focus:ring-1 focus:ring-primary",
+    "w-full border border-gray-primary rounded-lg bg-white p-2 text-sm font-normal text-gray-dark placeholder-black placeholder-opacity-20 outline-none transition-all focus:ring-1 focus:ring-primary",
     error ? "border-red-primary" : "",
     className
   );
@@ -20,7 +20,9 @@ const Input = (
   return (
     <div className="flex flex-col">
       <input {...rest} ref={ref} className={inputClassName} />
-      <small className="text-red-primary mt-1">{errorMessage}</small>
+      {error && errorMessage && (
+        <small className="text-red-primary mt-1">{errorMessage}</small>
+      )}
     </div>
   );
 };
